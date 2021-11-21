5330ce4972db76f7e44d74fd85caf2762bb1d5ee
Eduard Gavrila <edygavrila@gmail.com>
2021-11-21 20:41:46 +0000

    minor adjustments

--- components/Hello.js
+++ components/Hello.js
@@ -1,12 +1,9 @@
-
-
 const Hello = () => {
-    return (
-        <div>
-            Hello World!
-        </div>
-    )
-}
+  const useState = React.useState;
+  const [message, setMessage] = useState("");
+  if (message === "") {
+    setMessage("Hello, world!");
+  }
 
-
-
+  return <div className="message">{message}</div>;
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//This is the root React element. You can write your React app structure in this file.
 const App = () => {
+  //JSX code will be transpiled straight in the browser with the help of the babel script tag.
   return <div><Hello/></div>;
 };
 
