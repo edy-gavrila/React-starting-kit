4f3d1d6d306c05ba20f0758e0d5600c6b94e684d
Eduard Gavrila <edygavrila@gmail.com>
2021-11-21 21:10:09 +0000

    Some minor changes

--- components/Hello.js
+++ components/Hello.js
@@ -1,5 +1,6 @@
 const Hello = () => {
   const useState = React.useState;
+  //const useEffect = React.useEffect;
   const [message, setMessage] = useState("");
   if (message === "") {
     setMessage("Hello, world!");
